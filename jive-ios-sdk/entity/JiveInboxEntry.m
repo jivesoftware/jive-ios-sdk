76353b45bd5eccdbb0ce6d856deec04ca7ee295f
Rob Derstadt <rob.derstadt@jivesoftware.com>
2012-11-12 15:20:02 -0800

    experiment with afnetworking alias

--- jive-ios-sdk/entity/JiveInboxEntry.m
+++ jive-ios-sdk/entity/JiveInboxEntry.m
@@ -9,10 +9,18 @@
 #import "JiveInboxEntry.h"
 #import "JiveActivityObject.h"
 @implementation JiveInboxEntry
+
+
+
 @synthesize actor, content, generator, icon, jiveId, jive, object, openSocial, provider, published, target, title, updated, url, verb;
 
 - (NSString*) description {
     return [NSString stringWithFormat:@"%@ %@ -'%@'", self.object.url, self.verb, self.object.displayName];
 }
+
+
+
+
+
 
 @end
