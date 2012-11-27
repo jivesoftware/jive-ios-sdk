19faeb9f908210b466fcb0c771a098b5e07fea8d
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-11-27 11:13:33 -0700

    Moved request option class files into subfolder.

---
+++ jive-ios-sdk/RequestOptions/JivePageCountRequestOptions.h
@@ -0,0 +1,13 @@
+//
+//  JivePageCountRequestOptions.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 11/27/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JiveReturnFieldsRequestOptions.h"
+
+@interface JivePageCountRequestOptions : JiveReturnFieldsRequestOptions
+
+@end
---
+++ jive-ios-sdk/RequestOptions/JivePageCountRequestOptions.m
@@ -0,0 +1,13 @@
+//
+//  JivePageCountRequestOptions.m
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 11/27/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JivePageCountRequestOptions.h"
+
+@implementation JivePageCountRequestOptions
+
+@end
