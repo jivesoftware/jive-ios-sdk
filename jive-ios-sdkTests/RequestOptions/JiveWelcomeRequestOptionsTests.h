2058091a4d65094f65a7068739dc6ae9e6edddc9
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-11-27 16:34:14 -0700

    Added request option class for welcome requests.

---
+++ jive-ios-sdkTests/RequestOptions/JiveWelcomeRequestOptionsTests.h
@@ -0,0 +1,16 @@
+//
+//  JiveWelcomeRequestOptionsTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 11/27/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JiveReturnFieldsRequestOptionsTests.h"
+#import "JiveWelcomeRequestOptions.h"
+
+@interface JiveWelcomeRequestOptionsTests : JiveReturnFieldsRequestOptionsTests
+
+@property (readonly) JiveWelcomeRequestOptions *welcomeOptions;
+
+@end
