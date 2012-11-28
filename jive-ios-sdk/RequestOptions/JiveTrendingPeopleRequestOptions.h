268fb0223e7b6e6b045bc28e28bdb2fe128caaae
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-11-28 15:12:38 -0700

    Added request option class for trending people requests.

---
+++ jive-ios-sdk/RequestOptions/JiveTrendingPeopleRequestOptions.h
@@ -0,0 +1,15 @@
+//
+//  JiveTrendingPeopleRequestOptions.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 11/28/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JiveCountRequestOptions.h"
+
+@interface JiveTrendingPeopleRequestOptions : JiveCountRequestOptions
+
+@property (nonatomic, strong) NSURL *url;
+
+@end
