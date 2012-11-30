c4a9bdde024573baa56caf08276427e4dac5bc8f
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-11-30 16:14:16 -0700

    TABDEV-64: Added request option class for trending content requests. Updated trending people requests to suport trending content requests.

---
+++ jive-ios-sdkTests/RequestOptions/JiveTrendingContentRequestOptionsTests.h
@@ -0,0 +1,16 @@
+//
+//  JiveTrendingContentRequestOptionsTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 11/30/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JiveTrendingPeopleRequestOptionsTests.h"
+#import "JiveTrendingContentRequestOptions.h"
+
+@interface JiveTrendingContentRequestOptionsTests : JiveTrendingPeopleRequestOptionsTests
+
+@property (readonly) JiveTrendingContentRequestOptions *contentOptions;
+
+@end
