f052a79270498f540dd9b8a4f265a892221d4275
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-12-21 11:15:26 -0700

    TABDEV-64: Implemented toJSONDictionary for JiveLevel.

---
+++ jive-ios-sdkTests/entity/JiveLevelTests.h
@@ -0,0 +1,13 @@
+//
+//  JiveLevelTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 12/21/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JiveLevelTests : SenTestCase
+
+@end
