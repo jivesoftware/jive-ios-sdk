79d62512ab104af8f186a818ec7d9a57b789a969
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-12-28 15:59:45 -0700

    TABDEV-64: Implemented JivePost behavior.

---
+++ jive-ios-sdkTests/entity/JivePostTests.h
@@ -0,0 +1,16 @@
+//
+//  JivePostTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 12/28/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JiveContentTests.h"
+#import "JivePost.h"
+
+@interface JivePostTests : JiveContentTests
+
+@property (nonatomic, readonly) JivePost *post;
+
+@end
