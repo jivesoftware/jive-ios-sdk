15f453ec5f76cf4cda3bc53a1400971c78787431
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-12-28 18:28:12 -0700

    TABDEV-64: Implemented JiveGroup behavior.

---
+++ jive-ios-sdkTests/entity/JiveGroupTests.h
@@ -0,0 +1,16 @@
+//
+//  JiveGroupTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 12/28/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JivePlaceTests.h"
+#import "JiveGroup.h"
+
+@interface JiveGroupTests : JivePlaceTests
+
+@property (nonatomic, readonly) JiveGroup *group;
+
+@end
