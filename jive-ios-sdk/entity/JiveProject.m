3dfdaf6c9545d0cd3121c938882bfd825e6e4442
Orson Bushnell <orson.bushnell@jivesoftware.com>
2012-12-31 10:27:26 -0700

    TABDEV-64: Implemented JiveProject behavior.

--- jive-ios-sdk/entity/JiveProject.m
+++ jive-ios-sdk/entity/JiveProject.m
@@ -7,9 +7,38 @@
 //
 
 #import "JiveProject.h"
+#import "NSThread+JiveISO8601DateFormatter.h"
 
 @implementation JiveProject
 
 @synthesize creator, dueDate, projectStatus, startDate, tags;
+
+- (id)init {
+    if ((self = [super init])) {
+        self.type = @"project";
+    }
+    
+    return self;
+}
+
+- (NSDictionary *)toJSONDictionary {
+    NSMutableDictionary *dictionary = (NSMutableDictionary *)[super toJSONDictionary];
+    NSDateFormatter *dateFormatter = [NSThread currentThread].jive_ISO8601DateFormatter;
+    
+    [dictionary setValue:projectStatus forKey:@"projectStatus"];
+    if (tags)
+        [dictionary setValue:tags forKey:@"tags"];
+    
+    if (creator)
+        [dictionary setValue:[creator toJSONDictionary] forKey:@"creator"];
+    
+    if (dueDate)
+        [dictionary setValue:[dateFormatter stringFromDate:dueDate] forKey:@"dueDate"];
+    
+    if (startDate)
+        [dictionary setValue:[dateFormatter stringFromDate:startDate] forKey:@"startDate"];
+    
+    return dictionary;
+}
 
 @end
---
+++ jive-ios-sdkTests/entity/JiveProjectTests.h
@@ -0,0 +1,16 @@
+//
+//  JiveProjectTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 12/31/12.
+//  Copyright (c) 2012 Jive Software. All rights reserved.
+//
+
+#import "JivePlaceTests.h"
+#import "JiveProject.h"
+
+@interface JiveProjectTests : JivePlaceTests
+
+@property (nonatomic, readonly) JiveProject *project;
+
+@end
