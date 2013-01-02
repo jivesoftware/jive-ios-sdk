50d91c94c21442a7d6a8d20f8953d87c12fd29ff
Orson Bushnell <orson.bushnell@jivesoftware.com>
2013-01-02 11:46:27 -0700

    TABDEV-64: Added test files for JiveObject, no tests at this time.

---
+++ jive-ios-sdkTests/entity/JiveObjectTests.h
@@ -0,0 +1,13 @@
+//
+//  JiveObjectTests.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 1/2/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JiveObjectTests : SenTestCase
+
+@end
---
+++ jive-ios-sdkTests/entity/JiveObjectTests.m
@@ -0,0 +1,13 @@
+//
+//  JiveObjectTests.m
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 1/2/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import "JiveObjectTests.h"
+
+@implementation JiveObjectTests
+
+@end
