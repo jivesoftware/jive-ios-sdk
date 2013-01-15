954e669ad65ac9be421f9520c3bdbf9db5b9094e
Heath Borders <heath.borders@jivesoftware.com>
2013-01-15 16:39:44 -0600

    This API is required since we can't persist whole Jive objects in CoreData.
    Revert "Removed internal inbox api from Jive.h"
    
    This reverts commit f514f1b86f4091096c56bc747317d4028b6b1733.

--- jive-ios-sdk/Jive.h
+++ jive-ios-sdk/Jive.h
@@ -177,6 +177,7 @@
 - (void) inbox:(void(^)(NSArray*)) complete onError:(void(^)(NSError* error)) error;
 - (void) inbox:(JiveInboxOptions*) options onComplete:(void(^)(NSArray*)) complete onError:(void(^)(NSError* error)) error;
 - (void) markInboxEntries:(NSArray *)inboxEntries asRead:(BOOL)read onComplete:(void(^)(void))completeBlock onError:(void(^)(NSError *))errorBlock;
+- (void) markInboxEntryUpdates:(NSArray *)inboxEntryUpdates asRead:(BOOL)read onComplete:(void(^)(void))completeBlock onError:(void(^)(NSError *))errorBlock;
 
 #pragma mark - Environment
 
