d28c8d4c431509988d3ced2b058d8f7c68da0f33
Heath Borders <heath.borders@jivesoftware.com>
2013-01-18 09:57:59 -0700

    Added some debug statements.

--- jive-ios-sdk-tests/jive-ios-sdk-tests/cleanup-jive-framework-build.bash
+++ jive-ios-sdk-tests/jive-ios-sdk-tests/cleanup-jive-framework-build.bash
@@ -7,6 +7,8 @@
 #  Copyright (c) 2013 Jive Software. All rights reserved.
 
 #Cleanup Jive Framework build detritis
+echo Cleaning BUILT_PRODUCTS_DIR: "${BUILT_PRODUCTS_DIR}"
 pushd "${BUILT_PRODUCTS_DIR}"
+echo BUILT_PRODUCTS_DIR contents: `ls`
 rm -rf Jive/ JiveHeaders/ libJive.a
 popd
