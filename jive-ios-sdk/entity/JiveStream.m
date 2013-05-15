5f930623f9a1bfa3405c412b67ab4bb2c1ffff80
Chris Gummer <chris.gummer@jivesoftware.com>
2013-05-15 10:36:09 -0600

    TABDEV-1097: Add JiveStreamSourceValues to JiveStream.

--- jive-ios-sdk/entity/JiveStream.m
+++ jive-ios-sdk/entity/JiveStream.m
@@ -30,6 +30,15 @@ struct JiveStreamAttributes const JiveStreamAttributes = {
     .updated = @"updated"
 };
 
+struct JiveStreamSourceValues const JiveStreamSourceValues = {
+        .all = @"all",
+        .communications = @"communications",
+        .connections = @"connections",
+        .context = @"context",
+        .profile = @"profile",
+        .watches = @"watches"
+};
+
 @implementation JiveStream
 
 @synthesize jiveId, name, person, published, receiveEmails, resources, source, type, updated;
