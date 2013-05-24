40f1317b6d0a988ab5f8d3a34e0e438d03b022c3
Heath Borders <heath.borders@jivesoftware.com>
2013-05-24 15:44:03 -0500

    ENGSERV-2025. Need to install custom jive truststore.
    Install a custom truststore into the iOS Simulator that trusts anything signed with the ENG Root Cert.

---
+++ jive-ios-sdk-tests/jive-ios-sdk-tests/install-jive-truststore.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+#  install-jive-truststore.bash
+#  jive-ipad
+#
+#  Created by Heath Borders on 12/17/12.
+#  Copyright (c) 2012 Jive Software. All rights reserved.
+
+for TRUST_STORE_PATH in ~/Library/Application\ Support/iPhone\ Simulator/*/Library/Keychains/TrustStore.sqlite3; do
+echo overwriting $TRUST_STORE_PATH
+cp "$PROJECT_DIR"/"$TARGET_NAME"/jive-TrustStore.sqlite3 "$TRUST_STORE_PATH"
+done
