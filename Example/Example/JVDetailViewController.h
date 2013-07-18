de0615f7bee582f760f30f744f9ad35324cd9959
Orson Bushnell <orson.bushnell@jivesoftware.com>
2013-07-18 11:22:35 -0600

    TABDEV-1558, TABDEV-1561: Initial creation of an example project using the Jive iOS SDK.

---
+++ Example/Example/JVDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  JVDetailViewController.h
+//  Example
+//
+//  Created by Orson Bushnell on 7/18/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVDetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Orson Bushnell on 7/18/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JVAppDelegate class]));
+    }
+}
