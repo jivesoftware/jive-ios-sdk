0f4df97e42d67b7a4cf388a9becf3db1826bbf2d
Orson Bushnell <orson.bushnell@jivesoftware.com>
2013-07-18 11:43:38 -0600

    TABDEV-1558, TABDEV-1561: Added the login view controller.

---
+++ Example/Example/JVLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  JVLoginViewController.h
+//  Example
+//
+//  Created by Orson Bushnell on 7/18/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVLoginViewController : UIViewController
+
+@end
---
+++ Example/Example/JVLoginViewController.m
@@ -0,0 +1,17 @@
+//
+//  JVLoginViewController.m
+//  Example
+//
+//  Created by Orson Bushnell on 7/18/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import "JVLoginViewController.h"
+
+@interface JVLoginViewController ()
+
+@end
+
+@implementation JVLoginViewController
+
+@end
