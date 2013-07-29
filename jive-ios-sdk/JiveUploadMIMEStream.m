7176ebb53930a86e920907d13decd6fb8d9f6a21
Orson Bushnell <orson.bushnell@jivesoftware.com>
2013-07-29 11:42:39 -0600

    TABDEV-1684: Updated to implement the undocumented methods.

--- jive-ios-sdk/JiveUploadMIMEStream.m
+++ jive-ios-sdk/JiveUploadMIMEStream.m
@@ -9,6 +9,7 @@
 #import "JiveUploadMIMEStream.h"
 #import "JiveAttachment.h"
 #import "NSString+Jive.h"
+#import <objc/runtime.h>
 
 struct JiveUploadMIMEStreamElements const JiveUploadMIMEStreamElements = {
     .boundary = @"0xJiveBoundary",
@@ -19,19 +20,42 @@ struct JiveUploadMIMEStreamElements const JiveUploadMIMEStreamElements = {
     .fileDispositionFormat = @"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n"
 };
 
-@interface JiveUploadMIMEStream () <NSStreamDelegate>
+@interface JiveUploadMIMEStream () <NSStreamDelegate> {
+    CFRunLoopRef _cfRunLoop;
+    CFStringRef _cfMode;
+    CFReadStreamClientCallBack _clientCallback;
+    CFStreamClientContext      _clientContext;
+    CFOptionFlags              _clientFlags;
+}
 
 @property (nonatomic) NSInputStream *currentStream;
 @property (nonatomic) NSInputStream *boundaryStream;
 @property (nonatomic) NSInputStream *headerStream;
 @property (nonatomic) NSInteger fileIndex;
 @property (nonatomic) BOOL reading;
-@property (weak, nonatomic) NSRunLoop *currentRunLoop;
-@property (nonatomic) NSString *runLoopMode;
 
 @end
 
 @implementation JiveUploadMIMEStream
+
++ (BOOL)resolveInstanceMethod:(SEL)selector {
+    NSString *name = NSStringFromSelector(selector);
+    
+    if ([name hasPrefix:@"_"]) {
+        SEL aSelector = NSSelectorFromString([name substringFromIndex:1]);
+        Method method = class_getInstanceMethod(self, aSelector);
+        
+        if (method) {
+            class_addMethod(self,
+                            selector,
+                            method_getImplementation(method),
+                            method_getTypeEncoding(method));
+            return YES;
+        }
+    }
+    
+    return [super resolveInstanceMethod:selector];
+}
 
 - (NSInputStream *)boundaryStream {
     if (!_boundaryStream) {
@@ -46,13 +70,17 @@ struct JiveUploadMIMEStreamElements const JiveUploadMIMEStreamElements = {
 
 - (void)setCurrentStream:(NSInputStream *)currentStream {
     if (currentStream != _currentStream) {
-        _currentStream.delegate = nil;
-        [_currentStream close];
-        [_currentStream removeFromRunLoop:self.currentRunLoop forMode:self.runLoopMode];
+        if (_currentStream) {
+            _currentStream.delegate = nil;
+            [_currentStream close];
+            CFReadStreamScheduleWithRunLoop((CFReadStreamRef)_currentStream, _cfRunLoop, _cfMode);
+        }
         _currentStream = currentStream;
-        _currentStream.delegate = self;
-        [_currentStream scheduleInRunLoop:self.currentRunLoop forMode:self.runLoopMode];
-        [_currentStream open];
+        if (_currentStream) {
+            _currentStream.delegate = self;
+            CFReadStreamScheduleWithRunLoop((CFReadStreamRef)_currentStream, _cfRunLoop, _cfMode);
+            [_currentStream open];
+        }
     }
 }
 
@@ -70,15 +98,11 @@ struct JiveUploadMIMEStreamElements const JiveUploadMIMEStreamElements = {
 }
 
 - (void)scheduleInRunLoop:(NSRunLoop *)aRunLoop forMode:(NSString *)mode {
-    [self.currentStream removeFromRunLoop:self.currentRunLoop forMode:self.runLoopMode];
-    self.currentRunLoop = aRunLoop;
-    self.runLoopMode = mode;
-    [self.currentStream scheduleInRunLoop:aRunLoop forMode:mode];
+    [self scheduleInCFRunLoop:aRunLoop.getCFRunLoop forMode:(CFStringRef)mode];
 }
 
 - (void)removeFromRunLoop:(NSRunLoop *)aRunLoop forMode:(NSString *)mode {
-    [self.currentStream removeFromRunLoop:aRunLoop forMode:mode];
-    self.currentRunLoop = nil;
+    [self unscheduleFromCFRunLoop:aRunLoop.getCFRunLoop forMode:(CFStringRef)mode];
 }
 
 - (void)close {
@@ -144,6 +168,66 @@ struct JiveUploadMIMEStreamElements const JiveUploadMIMEStreamElements = {
     }
     
     [self.delegate stream:self handleEvent:eventCode];
+    if ( _clientCallback && eventCode & _clientFlags)
+        _clientCallback((__bridge CFReadStreamRef)self,
+                        (CFStreamEventType)eventCode,
+                        _clientContext.info);
+}
+
+#pragma mark Undocumented CFReadStream bridged methods
+
+- (void)scheduleInCFRunLoop:(CFRunLoopRef)aRunLoop forMode:(CFStringRef)aMode {
+    if (aRunLoop && aRunLoop != _cfRunLoop) {
+        CFRetain(aRunLoop);
+        CFRetain(aMode);
+        if (_cfRunLoop) {
+            CFRelease(_cfRunLoop);
+            CFRelease(_cfMode);
+        }
+        _cfRunLoop = aRunLoop;
+        _cfMode = aMode;
+        if (self.currentStream) {
+            CFReadStreamScheduleWithRunLoop((CFReadStreamRef)self.currentStream, aRunLoop, aMode);
+        }
+    }
+}
+
+- (BOOL)setCFClientFlags:(CFOptionFlags)flags
+                callback:(CFReadStreamClientCallBack)callback
+                 context:(CFStreamClientContext *)context {
+    if (context && context->version != 0)
+        return NO;
+    
+    if (_clientContext.release)
+        _clientContext.release(_clientContext.info);
+    
+    if (context) {
+        _clientContext = *context;
+        if (_clientContext.retain)
+            _clientContext.retain(_clientContext.info);
+        
+    } else {
+        CFStreamClientContext emptyContext;
+        
+        _clientContext = emptyContext;
+    }
+    
+    _clientFlags = flags;
+    _clientCallback = callback;
+    
+    return YES;
+}
+
+- (void)unscheduleFromCFRunLoop:(CFRunLoopRef)aRunLoop forMode:(CFStringRef)aMode {
+    if (aRunLoop && aRunLoop == _cfRunLoop) {
+        CFRelease(_cfRunLoop);
+        CFRelease(_cfMode);
+        _cfRunLoop = nil;
+        _cfMode = nil;
+        if (self.currentStream) {
+            CFReadStreamUnscheduleFromRunLoop((CFReadStreamRef)self.currentStream, aRunLoop, aMode);
+        }
+    }
 }
 
 @end
