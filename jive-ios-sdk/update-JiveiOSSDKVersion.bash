6856c4439fffdb94d4aeed735d3f111d327f6072
Orson Bushnell <orson.bushnell@jivesoftware.com>
2013-09-11 15:44:13 -0600

    TABDEV-2110: Update to not update the version number if a valid number cannot be found.

--- jive-ios-sdk/update-JiveiOSSDKVersion.bash
+++ jive-ios-sdk/update-JiveiOSSDKVersion.bash
@@ -2,15 +2,14 @@
 
 if [ $# -eq 1 ]; then
     build="$1"
-elif [ -d "$PROJECT_DIR"/.git ]; then
+elif [ -e "$PROJECT_DIR"/.git ]; then
     build=`git describe`
-else
-    build="SOURCE"
 fi
 
 if [[ "${build}" == "" ]]; then
 echo "No build number from git"
-exit 2
+exit
 fi
 
 echo "@\"${build}\"" > jive-ios-sdk/JiveiOSSDKVersion.h
+echo "PROJECT_NUMBER = ${build}" >> Documentation/Doxyfile
