32a90d0c90e4a9d031824c3108f6033f37df7057
Orson Bushnell <orson.bushnell@jivesoftware.com>
2014-01-15 16:49:38 -0700

    TABDEV-3196: Refactored test object creation into a TestObjectFactory method.

---
+++ jive-ios-sdkTests/TestObjectFactory.h
@@ -0,0 +1,13 @@
+//
+//  TestObjectFactory.h
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 1/15/14.
+//  Copyright (c) 2014 Jive Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObjectFactory : NSObject
+
+@end
---
+++ jive-ios-sdkTests/TestObjectFactory.m
@@ -0,0 +1,13 @@
+//
+//  TestObjectFactory.m
+//  jive-ios-sdk
+//
+//  Created by Orson Bushnell on 1/15/14.
+//  Copyright (c) 2014 Jive Software. All rights reserved.
+//
+
+#import "TestObjectFactory.h"
+
+@implementation TestObjectFactory
+
+@end
