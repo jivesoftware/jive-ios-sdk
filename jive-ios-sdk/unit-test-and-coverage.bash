5342a42ba6576e261ac5087f83db5bd759710dae
Orson Bushnell <orson.bushnell@jivesoftware.com>
2014-01-30 10:38:14 -0700

    TABDEV-3397: Changed the build script to work on Jenkins.

--- jive-ios-sdk/unit-test-and-coverage.bash
+++ jive-ios-sdk/unit-test-and-coverage.bash
@@ -7,6 +7,6 @@ rm -rf "$WORKSPACE_INTERNAL"/junit.xml
 rm -rf "$WORKSPACE_INTERNAL"/coverage.xml
 
 "$WORKSPACE_INTERNAL"/lib/xctool/xctool.sh -workspace "$WORKSPACE_INTERNAL"/jive-ios-sdk.xcworkspace/ -scheme jive-ios-sdk -sdk iphonesimulator6.1 -reporter junit:"$WORKSPACE_INTERNAL"/junit.xml -reporter plain test GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES OBJECT_FILE_DIR="$WORKSPACE_INTERNAL"/jive-ios-sdk/test-Objects >&1 ONLY_ACTIVE_ARCH=NO
-cp "$WORKSPACE_INTERNAL"/lib/gcovr "$WORKSPACE_INTERNAL"/test-Objects-*/i386
-cd "$WORKSPACE_INTERNAL"/test-Objects-*/i386
+cp "$WORKSPACE_INTERNAL"/lib/gcovr "$WORKSPACE_INTERNAL"/jive-ios-sdk/test-Objects-*/i386
+cd "$WORKSPACE_INTERNAL"/jive-ios-sdk/test-Objects-*/i386
 ./gcovr --xml --output "$WORKSPACE_INTERNAL"/coverage.xml -r "$WORKSPACE_INTERNAL"/jive-ios-sdk
